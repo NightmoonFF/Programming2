733a44ee07a18ad07b0910c40934585a65801155
elias <eliasasmark@hotmail.com>
2023-08-21 09:34:45 +0200

    Init Lketion 1

--- src/Lektion1_Git_og_superklasser/lektion1.java
+++ src/Lektion1_Git_og_superklasser/lektion1.java
@@ -2,5 +2,12 @@ package Lektion1_Git_og_superklasser;
 
 public class lektion1 {
 
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void test(){
+        System.out.println("test");
+    }
 
 }
