c075f467b9c63931de77186b9d9304615e0c8e1a
Nightmoon <eliasasmark@hotmail.com>
2023-09-02 19:53:45 +0200

    Opgave 3

---
+++ src/Lektion3_Interfaces/Opgave3/App.java
@@ -0,0 +1,8 @@
+package Lektion3_Interfaces.Opgave3;
+
+public class App {
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/Lektion3_Interfaces/Opgave3/Sellable.java
@@ -0,0 +1,6 @@
+package Lektion3_Interfaces.Opgave3;
+
+public interface Sellable {
+    double getSalesTaxPercent();
+    double getPriceAfterTax();
+}
